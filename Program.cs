6d78e6a62a8a64db60ebc09697c1debf68976b7b
Noe <VaescoNoe@gmail.com>
2019-04-23 23:08:49 -0500

    Variables implicitas

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+
+        var variable = 1;
+        var _variable = 'd';
+        var _variable0 = true;
+        var _variable1 = "Hola";
+        var _variable2 = 0.0;
+        var _variable3 = 0.0f;
+        var _variable4 = 0.0m;
+        
+        }
+    }
+
